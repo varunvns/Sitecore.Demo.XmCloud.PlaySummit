add11c18def90dedfa686e289c63f3ac4f0e93a0
vikassinghv34 <vikassinghv34@gmail.com>
2024-08-09 14:31:47 +0530

    API testing

--- src/rendering/src/pages/api/test.ts
+++ src/rendering/src/pages/api/test.ts
@@ -12,32 +12,28 @@ type Data = {
 };
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   console.log(req);
+  // const data = req.body;
   if (req.method === 'POST') {
-    const data = req.body;
-    if (data && data.email) {
-      console.log('Email data:', data.email);
-      res.status(200).json({ message: `Email received: ${data.email}` });
-      const fromAddress = 'kgholap@horizontal.com';
-      const toAddress = 'vthakur@horizontal.com';
-      const subject = 'Test Email';
-      const text = `Hey Vikas, This is a test email sent from Kunal for DP World POC work.\n Test Form email:- testing`;
-      try {
-        await sendgrid.send({
-          from: fromAddress,
-          to: toAddress,
-          subject: subject,
-          text: text,
-        });
-        console.log('Email sent successfully'); // Log on success
-        res.status(200).json({ message: 'Email sent successfully!' });
-      } catch (error) {
-        console.error('Error sending email:', error); // Log the error
-        res.status(500).json({ error: 'Failed to send email.' });
-      }
-    } else {
-      res.status(400).json({ message: 'Email not found in the request body' });
+    // console.log('Email data:', data.email);
+    // res.status(200).json({ message: `Email received: ${data.email}` });
+    const fromAddress = 'kgholap@horizontal.com';
+    const toAddress = 'vthakur@horizontal.com';
+    const subject = 'Test Email';
+    const text = `Hey Vikas, This is a test email sent from Kunal for DP World POC work.\n Test Form email:- testing`;
+    try {
+      await sendgrid.send({
+        from: fromAddress,
+        to: toAddress,
+        subject: subject,
+        text: text,
+      });
+      console.log('Email sent successfully'); // Log on success
+      res.status(200).json({ message: 'Email sent successfully!' });
+    } catch (error) {
+      console.error('Error sending email:', error); // Log the error
+      res.status(500).json({ error: 'Failed to send email.' });
     }
   } else {
-    res.status(405).json({ message: 'Method Not Allowed' });
+    res.status(400).json({ message: 'Email not found in the request body' });
   }
 }
